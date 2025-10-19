182e32b0744fc10671f53a29f5c4a050c70c1656
Vitor Lacerda <vitorlacerdafaria7@gmail.com>
2025-10-19 19:30:33 -0300

    fix(test): fix UserRepository integration tests using default context Users DbSet

--- template/backend/src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
@@ -8,6 +8,8 @@ namespace Ambev.DeveloperEvaluation.ORM;
 
 public class DefaultContext : DbContext
 {
+    public DbSet<User> Users { get; set; }
+
     public DefaultContext(DbContextOptions<DefaultContext> options) : base(options)
     {
     }
